7feba4e3e554f8121e6f2d8687b2bc011b686743
472146219742614 <145036637+472146219742614@users.noreply.github.com>
2023-09-14 18:27:22 +0300

    Update check.lua

--- check.lua
+++ check.lua
@@ -1,1 +1,1 @@
-local WhitelistedHWIDs = {loadstring(game:HttpGet("https://raw.githubusercontent.com/472146219742614/629145629147512784/main/hwid"))()}
+local WhitelistedHWIDs = loadstring(game:HttpGet("https://raw.githubusercontent.com/472146219742614/629145629147512784/main/hwid"))()
